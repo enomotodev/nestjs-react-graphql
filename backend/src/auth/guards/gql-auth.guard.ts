f0321864cffcae896207ecdb0b142038177bbf22
enomotodev <enomoto.dev@gmail.com>
2023-08-26 12:36:31 +0900

    Finished chapter.5 (finished backend project)

---
+++ backend/src/auth/guards/gql-auth.guard.ts
@@ -0,0 +1,16 @@
+import { AuthGuard } from '@nestjs/passport';
+import { ExecutionContext } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
+
+export class GqlAuthGuard extends AuthGuard('local') {
+  constructor() {
+    super()
+  }
+
+  getRequest(context: ExecutionContext) {
+    const ctx = GqlExecutionContext.create(context)
+    const request = ctx.getContext()
+    request.body = ctx.getArgs().signInInput
+    return request
+  }
+}
---
+++ backend/src/auth/strategies/local.strategy.ts
@@ -0,0 +1,21 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-local';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { AuthService } from '../auth.service';
+import { User } from '@prisma/client';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly authService: AuthService) {
+    super({ usernameField: 'email' })
+  }
+
+  async validate(email: string, password: string): Promise<User> {
+    const user = this.authService.validateUser(email, password)
+    if (!user) {
+      throw new UnauthorizedException()
+    }
+
+    return user
+  }
+}
